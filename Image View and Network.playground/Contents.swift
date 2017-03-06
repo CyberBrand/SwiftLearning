f52e6e2f049d75d3816316233878479a7eb698c2
Igor Smirnov <s@ronin.pro>
2017-03-07 01:44:04 +0300

    switch view creation

--- Image View and Network.playground/Contents.swift
+++ Image View and Network.playground/Contents.swift
@@ -3,12 +3,6 @@ import UIKit
 import PlaygroundSupport
 
 let container = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-view.layer.cornerRadius = 20.0
-view.layer.borderColor = UIColor.white.cgColor
-view.layer.borderWidth = 6.0
-view.backgroundColor = UIColor.green
-container.addSubview(view)
 let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
 if let data = try? Data(contentsOf: URL(string: "https://digitalhealthsummit.com/wp-content/uploads/2014/08/apple.jpg")!) {
     if let image = UIImage(data: data) {
@@ -16,7 +10,13 @@ if let data = try? Data(contentsOf: URL(string: "https://digitalhealthsummit.com
     }
 }
 container.addSubview(imageView)
-imageView
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+view.layer.cornerRadius = 20.0
+view.layer.borderColor = UIColor.red.cgColor
+view.layer.borderWidth = 6.0
+view.backgroundColor = UIColor.green
+container.addSubview(view)
 
 PlaygroundPage.current.liveView = container
 PlaygroundPage.current.needsIndefiniteExecution = true
